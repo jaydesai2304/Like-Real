8f48bcb0790730e1ce1e8c30314b0f302dea99a0
Jay Desai <jaydesai2304@gmail.com>
2024-07-30 10:50:51 +0530

    Add admin.py for admin site

--- glasses/lr/admin.py
+++ glasses/lr/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Person, Product, Address, Card, Contact,Cart
 
-# Register your models here.
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ("username", "fullname", "email", "phone", "gender")
+
+admin.site.register(Person, PersonAdmin)
